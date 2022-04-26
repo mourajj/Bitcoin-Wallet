7c7c9d8abf6ec8d947701777629c35fffd5977bf
Jonathan Moura <rlzpowmanolo@gmail.com>
2022-04-26 15:53:20 -0300

    Update balance.js

--- loadTesting/balance.js
+++ loadTesting/balance.js
@@ -5,11 +5,14 @@ import { sleep } from 'k6';
 export let options ={
     noConnectionReuse:false,
     vus: 1, //An integer value specifying the number of VUs (Virtual users) to run concurrently, used together with the iterations or duration options.
-    duration: '5s'
+    duration: '1s' //Duration of the test
 };
 
 export default function (){
 
-    http.get('http://127.0.0.1:5000/balance/jonathan')
+   // http.get("https://api.coinbase.com/v2/prices/USD-EUR/spot") If you want to test the delay of the external API
+
+    http.get('http://127.0.0.1:5000/balance/pedro') //Its recommended to specify an user that is present in the database, otherwise the call will not be properly made.
+    
     //sleep(1)
 }
